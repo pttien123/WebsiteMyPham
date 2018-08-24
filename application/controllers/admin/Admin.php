8ee2c3faec02afbc0c12fd14d2be8a4931f57f66
Tien Pham <1551010131tien@gmail.com>
2018-08-25 02:49:10 +0700

    Chinh sua loi

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -1,23 +1,33 @@
 <?php
 class Admin extends MY_Controller
 {
-    function __contruct()
+    function __construct()
     {
-        parent::__contruct();
+        parent::__construct();
         $this->load->model('admin_model');
     }
+
      /*
-     *  Lấy danh sách admin
+     *  Hiển thị danh sách các admin
      */
     function index()
     {
-      //Load dữ liệu view của trang
+      //Tạo các biến giữ giá trị trong bảng
+      $info = array();
+      $list = $this->admin_model->get_list($info);
+      //Biến cho biết tổng các hàng dữ liệu trong bảng
+      $total = $this->admin_model->get_total();
+      //Lấy nội dung thông báo bên function add()
       $message = $this->session->flashdata('message');
+      //Load dữ liệu view của trang
+      $this->data['list'] = $list;
+      $this->data['total'] = $total;
       $this->data['message'] = $message;
       $this->data['temp'] = 'admin/Admin/index';
       $this->load->view('admin/layoutmaster',$this->data);
     }
 
+    //Hàm kiểm tra dữ liệu của username có trùng hay không
     function check_username()
     {
         //Lấy dữ liệu cần kiểm tra
@@ -27,6 +37,7 @@ class Admin extends MY_Controller
         //Kiểm tra username đã tồn tại chưa
         if($this->admin_model->check_exists($where))
         {
+            //Nếu tồn tại
             //Trả về thông báo lỗi
             $this->form_validation->set_message(__FUNCTION__,'Username đã tồn tại');
             return FALSE;
@@ -38,50 +49,78 @@ class Admin extends MY_Controller
     */
     function add()
     {
-      //Load thư viện form_validation của CI
-      $this->load->library('form_validation');
-      $this->load->helper('form');
-      //Kiểm tra nếu có dữ liệu post thì sẽ kiểm tra dữ liệu có hợp lệ hay không
-      if($this->input->post())
-      {
-          $this->form_validation->set_rules('username','Username','required|max_length[30]|callback_check_username');
-          $this->form_validation->set_rules('ho','Họ và tên lót','required|max_length[50]');
-          $this->form_validation->set_rules('ten','Tên','required|max_length[20]');
-          $this->form_validation->set_rules('email','Email','required|valid_email');
-          $this->form_validation->set_rules('phone','Số điện thoại','required|max_length[12]');
-          $this->form_validation->set_rules('password','Mật khẩu','required|min_length[8]');
-          $this->form_validation->set_rules('repassword','Nhập lại mật khẩu','required|matches');
+        //Load thư viện form_validation của CI
+        $this->load->library('form_validation');
+        $this->load->helper('form');
+        //Khi nhấn nút submit -> kiểm tra dữ liệu nhập
+        if($this->input->post())
+        {
+            //giatri1: tên của input cần kiểm tra; giatri2: Nội dung xuất; giatri3: điều kiện kiểm tra
+            $this->form_validation->set_rules('username','Username','required|min_length[6]|callback_check_username');
+            $this->form_validation->set_rules('ho','Họ và tên lót','required|max_length[50]');
+            $this->form_validation->set_rules('ten','Tên','required|max_length[20]');
+            $this->form_validation->set_rules('email','Email','required|valid_email');
+            $this->form_validation->set_rules('phone','Số điện thoại','required|max_length[15]');
+            $this->form_validation->set_rules('password','Mật khẩu','required|min_length[8]|max_length[32]');
+            $this->form_validation->set_rules('repassword','Nhập lại mật khẩu','required|matches[password]');
 
-          //Nhập liệu chính xác
-          if($this->form_validation->run())
-          {
-              //Thêm vào cơ sở dữ liệu
-              //tạo biến lấy dữ liệu trong thẻ input
-              $username = $this->input->post('username');
-              $ho = $this->input->post('ho');
-              $ten = $this->input->post('ten');
-              $email = $this->input->post('email');
-              $phone = $this->input->post('phone');
-              $password = $this->input->post('password');
-              //Tạo biến data chứa mảng dữ liệu
-              $data = array(
-                //'tencot' => tenbien,
-              );
-              /*if($this->admin_model->insert($data)
-              {
-                  $this->session->set_flashdata('message','Thêm dữ liệu thành công');
-              }
-              else {
-                  $this->session->set_flashdata('message','Thêm dữ liệu thất bại');
-              }
-              //Chuyển về trang danh sách admin
-              redirect(admin_url('admin/index'));
-              */
-          }
-      }
-      //Load view của trang
-      $this->data['temp'] = 'admin/Admin/add';
-      $this->load->view('admin/layoutmaster',$this->data);
+            //có cái if này nó mới in mấy cái kiểm tra
+            //bên add.php form_error, form_value,...(không cần nội dung bên trong)
+            if($this->form_validation->run())
+            {
+                //Thêm mới hàng trong bảng
+                $username = $this->input->post('username');
+                $ho = $this->input->post('ho');
+                $ten = $this->input->post('ten');
+                $email = $this->input->post('email');
+                $phone = $this->input->post('phone');
+                $password = $this->input->post('password');
+                $today = date("Y/m/d");
+                $data = array(
+                    'Username' => $username,
+                    'Ho' => $ho,
+                    'Ten' => $ten,
+                    'Email' => $email,
+                    'Phone' => $phone,
+                    'Password' => $password,
+                    'NgayTao' => $today,
+                    'NgayCapNhat' => $today
+                );
+
+                if($this->admin_model->insert($data))
+                {
+                    //Tạo nội dung thông báo
+                    //giatri1: tên biến; giatri2: Nội dung biến
+                    $this->session->set_flashdata('message','Thêm thành công');
+                }
+                else
+                {
+                    $this->session->set_flashdata('message','Thêm thất bại');
+                }
+                //Chuyển về trang index
+                redirect(admin_url('admin'));
+            }
+
+        }
+        //Load view của trang
+        $this->data['temp'] = 'admin/Admin/add';
+        $this->load->view('admin/layoutmaster',$this->data);
     }
+
+    /*
+     * Chỉnh sửa dữ liệu một quản trị viên
+     */
+     function update()
+     {
+
+
+     }
+
+     function delete()
+     {
+       // code...
+     }
+
+
 }
  ?>
