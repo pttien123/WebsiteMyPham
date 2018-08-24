30445f7466ee97d8953e8f089f69716655d0f4a9
Tien Pham <1551010131tien@gmail.com>
2018-08-25 02:50:15 +0700

    Them ham moi

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -3,6 +3,16 @@
 //Chứa những hàm dùng chung cho cả admin và site
 function public_url($url='')
 {
-  return base_url('public/'.$url);
-  //Trả về đường dẫn localhost/webmypham/public/
+    return base_url('public/'.$url);
+    //Trả về đường dẫn localhost/webmypham/public/
+}
+
+function prt($list, $exit = true)
+{
+    echo "<pre>";
+    print_r($list);
+    if($exit)
+    {
+        die();
+    }
 }
