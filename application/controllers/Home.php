368a584e45c1c21f874d0b4cb4a1ceac3ace07a2
Tien Pham <1551010131tien@gmail.com>
2018-09-16 14:02:22 +0700

    Them phan title

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,6 +4,17 @@
 class Home extends MY_Controller{
     function index()
     {
+      //Lấy dữ liệu hình ảnh slide
+      // $this->load->model('slide_model');
+      // $list = $this->slide_model->get_list();
+      // $this->data['slide_list'] = $list;
+      //Lấy sản phẩm mới
+      $this->load->model('product_model');
+      $input = array();
+      $input['limit'] = array(3,0);
+      $new_product = $this->product_model->get_list($input);
+      $this->data['new_product'] = $new_product;
+      
       //Đường dẫn chứa nội dung chính hiển thị của trang --view/site/home/index
       $this->data['temp'] = 'site/Home/index';
       $this->data['title'] = 'Trang bán mỹ phẩm chất lượng';
