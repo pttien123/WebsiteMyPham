22f0d534dc162ed2faab9021d20c095b6886b220
Tien Pham <1551010131tien@gmail.com>
2018-09-30 22:59:20 +0700

    Phan gioi thieu va huong dan mua hang

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -15,7 +15,8 @@ class Home extends MY_Controller{
       $input['limit'] = array(3,0);
       $new_product = $this->product_model->get_list($input);
       $this->data['new_product'] = $new_product;
-
+      // $input['where'] = array('KhuyenMai !=' =>0);
+      // $discount_product =  $this->product_model->get_list($input);
       //Đường dẫn chứa nội dung chính hiển thị của trang --view/site/home/index
       $this->data['temp'] = 'site/Home/index';
       $this->data['title'] = 'Trang bán mỹ phẩm chất lượng';
@@ -23,6 +24,20 @@ class Home extends MY_Controller{
       $message = $this->session->flashdata('message');
       $this->data['message'] = $message;
 
+      //Dùng để load view của trang --view site/layoutmaster
+      $this->load->view('site/layoutmaster',$this->data);
+    }
+    function about()
+    {
+      $this->data['temp'] = 'site/Home/about';
+      $this->data['title'] = 'Giới thiệu';
+      //Dùng để load view của trang --view site/layoutmaster
+      $this->load->view('site/layoutmaster',$this->data);
+    }
+    function guide()
+    {
+      $this->data['temp'] = 'site/Home/guide';
+      $this->data['title'] = 'Hướng dẫn mua hàng';
       //Dùng để load view của trang --view site/layoutmaster
       $this->load->view('site/layoutmaster',$this->data);
     }
